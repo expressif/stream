2b57ff21ca5087eea0d1f1f0b17520b702fce46a
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-01-25 18:44:25 +0100

    the event class now handles read & write streams

--- src/Expressif/Stream/Event.php
+++ src/Expressif/Stream/Event.php
@@ -9,51 +9,120 @@ namespace Expressif\Stream {
   /**
    * Event wrapper
    */
-  class Event extends EventEmitter {
+  class Reader extends EventEmitter {
 
-    public $event;
+    /**
+     * @var boolean Indicates if you can write into the stream
+     */
+    public $writable = false;
+    /**
+     * @var boolean Indicates if you can read data from the stream
+     */
+    public $readable = false;
+    /**
+     * The current stream
+     */
     public $stream;
-    protected $flags = EV_READ | EV_PERSIST;
+
+    private $ev_read;
+    private $ev_write;
+
     /**
      * Initialize a new stream listener
      */
     public function __construct($stream) {
+
       $this->stream = $stream;
       stream_set_blocking($this->stream, 0);
-      $this->event = event_new();
-      event_set($this->event, $this->stream, $this->flags, array($this, '_trigger'));
-      Loop::attachEvent($this->event);
+      $meta = stream_get_meta_data($this->stream);
+
+      if (substr($meta['mode'], -1) === '+') {
+        $this->writable = true;
+        $this->readable = true;
+      } else{
+        if (strpos($meta['mode'], 'r') !== false) {
+          $this->readable = true;
+        }
+        if (strpos($meta['mode'], 'w') !== false) {
+          $this->writable = true;
+        }
+      }
+
+      if ($this->readable) {
+        $this->ev_read = event_new();
+        event_set($this->ev_read, $this->stream, EV_READ | EV_PERSIST, array($this, '_read'));
+        Loop::attachEvent($this->ev_read);
+      }
+
+      if ($this->writable) {
+        $this->ev_write = event_new();
+        event_set($this->ev_write, $this->stream, EV_WRITE | EV_PERSIST, array($this, '_write'));
+        Loop::attachEvent($this->ev_write);
+      }
+
     }
 
     /**
-     * Free from loop
+     * Free from loop and close the stream
      */
     public function __destruct() {
-      if (!empty($this->event)) {
-        event_del($this->event);
-        event_free($this->event);
-        unset($this->stream, $this->event);
+      if (!empty($this->ev_read)) {
+        event_del($this->ev_read);
+        event_free($this->ev_read);
+        unset($this->ev_read);
+      }
+      if (!empty($this->ev_write)) {
+        event_del($this->ev_write);
+        event_free($this->ev_write);
+        unset($this->ev_write);
+      }
+      if (!empty($this->stream)) {
+        fclose($this->stream);
+        unset($this->stream);
       }
     }
 
     /**
      * Entry point for read event
      */
-    public function _trigger() {
+    public function _read() {
       if (feof($this->stream)) {
         $this->close();
       } else {
-        $this->emit('trigger');
+        $this->emit('read', [fread($this->stream, 4048)]);
       }
     }
 
+    /**
+     * Writes some data to the stream
+     */
+    public function write($data) {
+      if (!$this->writable) {
+        throw new \Exception('The stream is not writable');
+      }
+      fwrite($this->stream, $data);
+      return $this;
+    }
+
+    /**
+     * Entry point for read event
+     */
+    public function _write() {
+      if (empty($this->stream)) return;
+      if (feof($this->stream)) {
+        $this->close();
+      } else {
+        $this->emit('write');
+      }
+    }
 
     /**
      * Closing the current stream
      */
     public function close() {
+      $this->readable = false;
+      $this->writable = false;
       $this->emit('close');
-      fclose($this->stream);
       $this->__destruct();
       return $this;
     }
